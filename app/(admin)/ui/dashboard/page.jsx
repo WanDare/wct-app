a418d283ff807d5ef10caab5748b38f53955d9de
Neth Wandara <ytdare51@gmail.com>
2024-01-23 14:59:49 +0700

    added pie chart

--- app/(admin)/ui/dashboard/page.jsx
+++ app/(admin)/ui/dashboard/page.jsx
@@ -4,10 +4,16 @@ import Card from "../card/card";
 import CardLineChart from "../charts/chart2";
 import CardBarChart from "../charts/chart3";
 import { Card2 } from "../card/card2";
+import PieActiveArc from "../charts/piechart";
+import BarsDataset from "../charts/chart4";
 
 const DashboardsUi = () => {
   return (
     <div>
+      <div className="flex flex-wrap gap-10 my-5">
+        <PieActiveArc />
+        <BarsDataset />
+      </div>
       <CardLineChart />
       <Users />
       <CardBarChart />
