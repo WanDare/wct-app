92bfb671e3cf5bc097b4f19cce96b23c1b2892b5
Neth Wandara <ytdare51@gmail.com>
2025-01-20 16:13:28 +0700

    Backend configuration

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARSvcS2Bfkp7Gj9hEqSXaUj8QaZREOoQ8",
-  authDomain: "next-firebase-10bc3.firebaseapp.com",
-  projectId: "next-firebase-10bc3",
-  storageBucket: "next-firebase-10bc3.appspot.com",
-  messagingSenderId: "712292943588",
-  appId: "1:712292943588:web:dd849cc6f163c410f40ef1",
-  measurementId: "G-GVSVLLXDZ1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
